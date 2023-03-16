3e2a598d3f1d9db2215063c95a2da2c8430b01b2
Casey Kelly <cmk0811@uw.edu>
2023-03-16 15:32:24 -0700

    final submission

--- project/app.py
+++ project/app.py
@@ -17,18 +17,19 @@ def results():
     if request.method == 'POST':
         current_location = request.form['curr_location_input']
         direction = request.form['drop_down']
-        radius = request.form['radius_input']
-        nearby_lrstops = linkright.next_train(direction, radius)
-        for station in nearby_lrstops:
-            station_key = station
-            dist_to_station = nearby_lrstops[0]
-            time_to_station = nearby_lrstops[1]
-            next_trains = [nearby_lrstops[2], nearby_lrstops[3], nearby_lrstops[4]]
-        return render_template('results.html', current_location=current_location, station_key=station_key, dist_to_station=dist_to_station, time_to_station=time_to_station, next_trains=next_trains)
+        radius = float(request.form['radius_input'])
+        nearby_lrstops = linkright.next_train(direction, radius, current_location)
+        station_count = len(nearby_lrstops)
+        # for station_key, stop_info in nearby_lrstops.items():
+        #     dist_to_station = stop_info[0]
+        #     time_to_station = stop_info[1]
+        #     next_trains = [stop_info[2], stop_info[3], stop_info[4]]
+        return render_template('results.html', current_location=current_location, nearby_lrstops=nearby_lrstops, radius=radius, station_count=station_count)
     else:
         return 'Error: was expecting a POST request', 400
 
 
+app.run(debug=True)
 
 
-
+#station_key=station_key, dist_to_station=dist_to_station, time_to_station=time_to_station, next_trains=next_trains
--- project/linkright.py
+++ project/linkright.py
@@ -9,7 +9,7 @@ from math import floor
 #Pioneer Square station stop ID NOT working   #lr_stops: { 'station' : [ lon, lat, NB stop, SB stop]
 lr_stops = {'Northgate Station': [47.701976, -122.328388, '1_990006', '1_990005'], 'Roosevelt Station': [47.676594, -122.315985, '1_990004', '1_990003'],
             'U District Station': [47.660312, -122.314131, '1_990002', '1_990001'], 'University of Washington Station': [47.649704, -122.303772, '1_99605', '1_99604'], 'Capitol Hill Station': [47.619591, -122.320214, '1_99603', '1_99610'],
-            'Westlake Station': [47.611597, -122.336666, '1_1121', '1_1108'], 'University Street Station': [47.607746, -122.335960, '1_565', '1_455'], 'Pioneer Square Station': [47.602669, -122.331318, '1_501'],
+            'Westlake Station': [47.611597, -122.336666, '1_1121', '1_1108'], 'University Street Station': [47.607746, -122.335960, '1_565', '1_455'], 'Pioneer Square Station': [47.602669, -122.331318,  '1_532', '1_501'],
             'International District/Chinatown Station': [47.598055, -122.328019, '1_621', '1_623'], 'Stadium Station': [47.592047, -122.327171, '1_99260', '1_99101'], 'SODO Station': [47.580271, -122.327393, '1_99256', '1_99111'],
             'Beacon Hill Station': [47.579401, -122.311340, '1_99240', '1_99121'], "Mount Baker Station": [47.576716, -122.297802, '1_55860', '1_55949'], 'Columbia City Station': [47.559616, -122.292618, '1_55778', '1_56039'],
             'Othello Station': [47.538006, -122.281574, '1_55656', '1_56159'], 'Rainier Beach Station': [47.522610, -122.279335, '1_55578', '1_56173'], 'Tukwila International Boulevard Station': [47.464098, -122.288201, '1_99905', '1_99900'],
@@ -18,7 +18,6 @@ lr_stops = {'Northgate Station': [47.701976, -122.328388, '1_990006', '1_990005'
 
 ##RETURN LAT AND LONG LIST FOR CURRENT LOCATION (INPUT)##
 def geocode_current_location(current_location):
-    current_location = 'University of Washington'
     geocode_args = {'text': current_location, 'apiKey': keys.GEOAPIFY_SECRET_API_KEY}
     geocode_paramstr = urllib.parse.urlencode(geocode_args)
     geocode_baseurl = 'https://api.geoapify.com/v1/geocode/search?'
@@ -34,8 +33,8 @@ def geocode_current_location(current_location):
 #FINDS STATIONS WITHIN RADIUS, RETURNS Dict OF NEARBY STATIONS {stop name: dist (mi), time to dist (min)}
 
 
-def get_time_dist_to_lrstops(lr_stops, radius):
-    current_location = geocode_current_location()
+def get_time_dist_to_lrstops(lr_stops, radius, current_location):
+    current_location = geocode_current_location(current_location)
     nearby_lrstops = {}
 
     for station in lr_stops:
@@ -63,11 +62,8 @@ def get_time_dist_to_lrstops(lr_stops, radius):
     return nearby_lrstops
 
 
-
-
-
-def next_train(lr_stops, direction, radius):
-    nearby_lrstops = get_time_dist_to_lrstops(lr_stops, radius)
+def next_train(direction, radius, current_location):
+    nearby_lrstops = get_time_dist_to_lrstops(lr_stops, radius, current_location)
 
     for station in nearby_lrstops:
         # baseurl = 'http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/'
